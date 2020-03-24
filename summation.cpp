b70c32aafc608e221ac5395fd96b4244d7e788d0
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2020-03-24 10:20:28 -0400

    Initial commit

---
+++ summation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <vector>
+
+using std::vector;
+
+vector<int> sum(vector<int>& a1, vector<int>& a2) {
+  if (size(a1) != size(a2)) {
+    throw std::exception();
+  }
+
+  int n = size(a1);
+  vector<int> result(n,0);
+  for (int i=0; i!=n; ++i) {
+    result[i] = a1[i] + a2[i];
+  }
+
+  return result;
+}
+
+int main() {
+  vector<int> a1 = { {1,2,3,4} };
+  vector<int> a2 = { {4,3,2,1} };
+
+  vector<int> output = sum(a1, a2);
+  for (auto el: output) {
+    std::cout << el << " ";
+  }
+  std::cout << '\n';
+
+  std::ofstream myfile;
+  myfile.open("summation.txt");
+  for (auto el: output) {
+    myfile << el << " ";
+  }
+  
+
+  return 0;
+}
